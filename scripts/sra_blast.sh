cb6fdcb1a27aae821e4bdec0b681306242744433
Ubuntu <barb.bryant@gmail.com>
2018-07-12 21:04:49 +0000

    Adding herv database to git. Also new sra_blast file

---
+++ scripts/sra_blast.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -x # for debuging
+
+# FIXME: MAKE cancer type dynamic
+Cancer_Type="AML"
+Species_Type="Homo sapiens"
+
+NUM_JOBS=50 # Number of total jobs to run
+NUM_PARALLEL_JOBS=4 # Number of jobs to run in parallel
+
+DATADIR="data"
+
+HERV_DB="${DATADIR}/herv/herv_ref_135.fasta"
+
+# Create DIRECTORY for Cancer Type
+mkdir -p ${DATADIR}/${Cancer_Type}
+
+# Create Results sub-directory
+RESULTS_DIR="$DATADIR}/${Cancer_Type}/results"
+mkdir -p ${RESULTS_DIR}
+
+# Create working sub-directory
+WORKING_DIR="${DATADIR}/${Cancer_Type}/working"
+mkdir -p "${WORKING_DIR}"
+
+# FILENAMES for intermediate results
+EFETCH_FILE=${WORKING_DIR}/${Cancer_Type}_efetch.txt
+RUN_ID_FILE=${WORKING_DIR}/${Cancer_Type}_run_id.txt
+PARALLEL_CMD_FILE="${WORKING_DIR}/magicblast_commands.txt"
+
+OUTPUT_EXT="_res.sam"
+
+Query_String="$Cancer_Type[All Fields] AND \"$Species_Type\"[orgn] AND (cluster_public[prop] AND \"biomol rna\"[Properties])"
+
+# Make the blast db 
+# FIXME: Should only be done once and not per Cancer_Type
+makeblastdb -in $HERV_DB -out retro_virus_db -parse_seqids -dbtype nucl
+
+# Get the metadata from SRA archive
+esearch -db sra -query "$Query_String"  | efetch -format runinfo  > ${EFETCH_FILE}
+
+# Filter the metadata so that we remove headers and also cell lines
+cat ${EFETCH_FILE} | grep -v '^$' \
+       | grep -v '^Run' \
+       | grep -iv "cell line" \
+       | grep -i ${Cancer_Type} \
+       | awk -F',' '{print $1}' > ${RUN_ID_FILE}
+
+
+# Pick up all the resulting Run ID's and put it into an array
+run_ids_array=(`cat ${RUN_ID_FILE}`)
+
+# Create a parallel command file
+echo -n "" > ${PARALLEL_CMD_FILE}
+
+# Output a magic blast command to the parallel command file
+# We have one command for each Run ID
+for run_id in "${run_ids_array[@]}"
+do
+   echo "$run_id"
+   echo "magicblast -sra ${run_id}  -db retro_virus_db -no_discordant -num_threads 4 -no_unaligned -out ${RESULTS_DIR}/${run_id}_res.sam" >> ${PARALLEL_CMD_FILE}
+done
+
+# Read the first NUM_JOBS from the command file and run them in parallel 
+# making sure that only NUM_PARALLEL_JOBS are run at a given time
+head -{NUM_JOBS} ${PARALLEL_CMD_FILE} | parallel -j ${NUM_PARALLEL_JOBS}
