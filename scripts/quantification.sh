de0fb9256bb3cd06cd0aa09e5a150db755e53a6b
Kishore Anekalla <kishorereddyanekalla@gmail.com>
2018-07-13 09:03:33 -0500

    tested on other samples

--- scripts/quantification.sh
+++ scripts/quantification.sh
@@ -5,6 +5,7 @@
 for i in `ls *.sam` ; 
     do 
     samtools view -S -b $i > $i.bam;
+    samtools index $i.bam;
     samtools sort $i.bam -o $i.sorted.bam;
 done;
 
@@ -16,3 +17,7 @@ for i in `ls *.bam` ;
     do 
     samtools view -F 260 $i | cut -f 3 | sort | uniq -c | awk '{printf("%s\t%s\n", $2, $1)}' > $i.counts.txt
 done;
+
+# merging files
+#awk 'FNR==NR{a[$1]=$2 FS $3;next}{ print $0, a[$1]}' counts2.txt counts1.txt
+# coverage estimation 
